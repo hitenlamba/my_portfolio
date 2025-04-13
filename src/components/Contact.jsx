1ed030b21abb194887f15b19aefbecbb373144a1
hitenlamba <hitenlamba00@gmail.com>
2025-04-13 14:59:32 -0400

    uptill website deploment first version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,28 +6,26 @@ const ImageClipBox = ({ src, clipClass }) => (
     <img src={src} />
   </div>
 );
-
 const Contact = () => {
   return (
-    <div id="contact" className="my-20 min-h-96 w-screen  px-10">
-      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
-        <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
+    <div id="contact" className="my-20 min-h-96 w-screen px-10">
+      <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden px-5">
+        <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96 sm:left-10 sm:right-10">
           <ImageClipBox
             src="/img/Contact1.jpeg"
-            clipClass="contact-clip-path-1"
+            clipClass="contact-clip-path-1 sm:scale-90 lg:scale-100"
           />
-
           <ImageClipBox
             src="/img/Contact1.jpeg"
-            clipClass="contact-clip-path-2"
+            clipClass="contact-clip-path-2 sm:scale-90 lg:scale-100"
           />
           <ImageClipBox
             src="/img/Contact3.jpeg"
-            clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
+            clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60 sm:scale-75"
           />
         </div>
 
-        <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
+        <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80 sm:left-10 sm:right-10">
           <ImageClipBox
             src="/img/Contact4.jpeg"
             clipClass="sword-man-clip-path md:scale-125"
@@ -54,5 +52,3 @@ const Contact = () => {
     </div>
   );
 };
-
-export default Contact;
